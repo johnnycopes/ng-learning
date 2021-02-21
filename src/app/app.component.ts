ca12ec2e0c3ab07c4bbefc0ed1b708dc0a1c7738
John Coppola <jcoppola@insiten.com>
2021-02-21 10:40:18 -0500

    sets up demos.ts file and basic routing

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,8 +1,11 @@
 import { Component } from '@angular/core';
+import { DEMOS } from "./demos/demos";
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.scss']
 })
-export class AppComponent { }
+export class AppComponent {
+  public demos = DEMOS;
+}
---
+++ src/app/demos/demos.ts
@@ -0,0 +1,51 @@
+import { HoldableDemoComponent } from "./holdable-demo/holdable-demo.component";
+import { NgTemplateOutletDemoComponent } from "./ng-template-outlet-demo/ng-template-outlet-demo.component";
+import { StructuralDirectivesComponent } from "./structural-directives/structural-directives.component";
+import { HeadlessComponentsComponent } from "./headless-components/headless-components.component";
+import { GlobalEventListenersDemoComponent } from "./global-event-listeners-demo/global-event-listeners-demo.component";
+
+interface IDemo {
+  name: string;
+  path: string;
+  description: string;
+  resource: string;
+  component: unknown;
+}
+
+export const DEMOS: IDemo[] = [
+  {
+    name: "Holdable Directive",
+    path: "01-holdable",
+    description: "blah",
+    resource: "blah",
+    component: HoldableDemoComponent,
+  },
+  {
+    name: "NgTemplateOutlet Customization",
+    path: "02-ng-template-outlet",
+    description: "blah",
+    resource: "blah",
+    component: NgTemplateOutletDemoComponent,
+  },
+  {
+    name: "Structural Directives",
+    path: "03-structural-directives",
+    description: "blah",
+    resource: "blah",
+    component: StructuralDirectivesComponent,
+  },
+  {
+    name: "Headless Angular Components",
+    path: "04-headless-components",
+    description: "blah",
+    resource: "blah",
+    component: HeadlessComponentsComponent,
+  },
+  {
+    name: "Global Event Listeners",
+    path: "05-global-event-listeners",
+    description: "blah",
+    resource: "blah",
+    component: GlobalEventListenersDemoComponent,
+  }
+];
